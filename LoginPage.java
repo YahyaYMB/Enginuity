1b2cadfcdf46141abcbbec306db5d6974e453a22
ZainBarlas0 <zainbarlas0@gmail.com>
2026-02-13 12:37:56 -0500

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -1,5 +1,6 @@
 package com.example.EduPlan.ui;
 
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -8,19 +9,28 @@ import com.vaadin.flow.router.Route;
 public class LoginPage extends VerticalLayout {
 
     public LoginPage(){
-    
+        
         setSizeFull(); 
 
        
         setJustifyContentMode(JustifyContentMode.CENTER); 
         setAlignItems(Alignment.CENTER);
-        
-        LoginForm loginForm = new LoginForm();
-        
-       
 
-       
-        add(loginForm);
+      Html backgroundVideo = new Html(
+            "<video autoplay loop muted playsinline " +
+            "style='position: fixed; top: 0; left: 0; min-width: 100%; min-height: 100%; " +
+            "z-index: -1; object-fit: cover;'>" +
+            " <source src='space-bg.mp4' type='video/mp4'>" +
+            "</video>"
+        );
+
+        // 2. Style the Login Form to look better against dark space
+        LoginForm loginForm = new LoginForm();
+        loginForm.getElement().getStyle().set("background", "rgba(255, 255, 255, 0.9)");
+        loginForm.getElement().getStyle().set("border-radius", "15px");
+        loginForm.getElement().getStyle().set("padding", "10px");
+
+        add(backgroundVideo, loginForm);
     }
 
-}
+}//ph premium
