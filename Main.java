183a48c53043c1d159da37698f0964c5f7bd854d
ZainBarlas0 <zainbarlas0@gmail.com>
2026-02-13 12:49:44 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.example.EduPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+
+@SpringBootApplication
+@StyleSheet("styles.css")
+public class EduPlanApplication implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduPlanApplication.class, args);
+    }
+}
